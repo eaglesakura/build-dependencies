cbdc39eec30e6b53071591f6cddf92e1b32427d9
eaglesakura <eagle.sakura@gmail.com>
2016-02-04 11:33:46 +0900

    アップデートスクリプトの実行方法を修正

--- android-sdk-install-circle.sh
+++ android-sdk-install-circle.sh
@@ -20,4 +20,6 @@ echo "###################################"
 echo "#    update platform tools        #"
 echo "###################################"
 export ANDROID_HOME=$REQUEST_SDK_PATH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/eaglesakura/build-dependencies/master/android-sdk.sh)"
+wget -O temp.sh "https://raw.githubusercontent.com/eaglesakura/build-dependencies/master/android-sdk-install-circle.sh"
+chmod 755 temp.sh
+source ./temp.sh
