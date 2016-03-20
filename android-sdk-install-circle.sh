bdad60d922c5ede101fdc2a84eb630304d50f4b9
eaglesakura <eagle.sakura@gmail.com>
2016-03-20 21:14:46 +0900

    依存バージョンを修正

--- android-sdk-install-circle.sh
+++ android-sdk-install-circle.sh
@@ -7,7 +7,7 @@ else
   echo "ANDROID_HOME=$ANDROID_HOME"
 fi
 
-DEPENDENCIES_VERSION=20160320b
+DEPENDENCIES_VERSION=20160320c
 TEMP_INSTALL_PATH=$ANDROID_HOME.tmp
 SDK_URL=https://dl.google.com/android/android-sdk_r23-linux.tgz
 
