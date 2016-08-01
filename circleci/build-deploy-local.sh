5f661e8d4e0155f864cc5d82c0ed44b1100f4707
eaglesakura <eagle.sakura@gmail.com>
2016-08-01 15:19:16 +0900

    ログ位置を修正

--- circleci/build-deploy-local.sh
+++ circleci/build-deploy-local.sh
@@ -3,8 +3,8 @@
 ./gradlew clean build javadoc uploadArchives uploadJavadoc
 
 if [ $? -ne 0 ]; then
-    echo "Build Failed."
     cp -r ./build/reports/ "$CIRCLE_ARTIFACTS"
     cp -r ./build/outputs/ "$CIRCLE_ARTIFACTS"
+    echo "Build Failed."
     exit 1
 fi
