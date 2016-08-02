a19d222505c9cb4f02c65a2e60ed7385d7bf4aac
eaglesakura <eagle.sakura@gmail.com>
2016-08-02 15:38:29 +0900

    API単位でのインストールスクリプトを追加

---
+++ circleci/install-android-api10.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+install_platform() {
+echo "check build-tools-$1"
+
+if [ -e "$ANDROID_HOME/platforms/android-$1" ]; then
+  echo "  * Installed SDK $1"
+  return
+fi
+
+echo y | "$ANDROID_HOME/tools/android" update sdk -u -a -t "android-$1"
+
+}
+
+install_platform "10"
