5f403ca610670d82fc57628528ee6cb656d487e6
eaglesakura <eagle.sakura@gmail.com>
2016-08-02 13:30:00 +0900

    lint errorを表示するように修正

--- circleci/build-deploy-local.sh
+++ circleci/build-deploy-local.sh
@@ -5,6 +5,11 @@
 if [ $? -ne 0 ]; then
     cp -r ./build/reports/ "$CIRCLE_ARTIFACTS"
     cp -r ./build/outputs/ "$CIRCLE_ARTIFACTS"
+
+    echo "##############################################"
+    echo "#               ERROR REPORT                 #"
+    echo "##############################################"
+    cat outputs/lint-results-debug.xml
     echo "Build Failed."
     exit 1
 fi
