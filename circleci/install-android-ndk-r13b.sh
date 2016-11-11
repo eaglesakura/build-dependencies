eed7bc717523e39255e96041e08897a788c96ef2
eaglesakura <eagle.sakura@gmail.com>
2016-11-11 17:58:49 +0900

    Revisionコントロールを変数に変更

--- circleci/install-android-ndk-r13b.sh
+++ circleci/install-android-ndk-r13b.sh
@@ -7,9 +7,11 @@ else
   echo "ANDROID_NDK_HOME=$ANDROID_NDK_HOME"
 fi
 
-SDK_URL=https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
+DEPENDENCIES_VERSION = r13b
+SDK_URL="https://dl.google.com/android/repository/android-ndk-${DEPENDENCIES_VERSION}-linux-x86_64.zip"
+
 echo "#############################################"
-echo "#    Download Android SDK rev. ${DEPENDENCIES_VERSION} #"
+echo "#    Download Android NDK rev. ${DEPENDENCIES_VERSION} #"
 echo "#############################################"
 if [ -e "$ANDROID_NDK_HOME" ]; then
     echo "Downloaded android ndk"
@@ -20,5 +22,5 @@ else
     mkdir "/home/ubuntu/android-ndk.tmp"
     cd "/home/ubuntu/android-ndk.tmp"
     unzip "/home/ubuntu/android-ndk.zip"
-    mv "/home/ubuntu/android-ndk.tmp/android-ndk-r13b" $ANDROID_NDK_HOME
+    mv "/home/ubuntu/android-ndk.tmp/android-ndk-${DEPENDENCIES_VERSION}" $ANDROID_NDK_HOME
 fi
